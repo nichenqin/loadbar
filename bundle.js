3b273a62f055ef73bbf752b3d3907efe2e88eee1
nichenqin1001 <nichenqin@hotmail.com>
2017-06-11 13:28:07 +0800

    map style to element

--- bundle.js
+++ bundle.js
@@ -32,26 +32,51 @@ var isHTMLElement = function isHTMLElement(el) {
   return el instanceof HTMLElement;
 };
 
+var mapStyleToElement = function mapStyleToElement(el, style) {
+  for (var prop in style) {
+    el.style[prop] = style[prop];
+  }
+};
+
 var LoadingBar = function () {
   function LoadingBar(el, options) {
     classCallCheck(this, LoadingBar);
 
     this.el = typeof el === 'string' ? document.querySelector(el) : el;
-    this.options = options || {};
+
+    var defaultOptions = {
+      style: {
+        height: '4px',
+        backgroundColor: 'orange'
+      }
+    };
+
+    this.options = Object.assign({}, defaultOptions, options);
 
     this._init();
   }
 
   createClass(LoadingBar, [{
     key: '_init',
-    value: function _init() {}
+    value: function _init() {
+      !isHTMLElement(this.el) && this._createElement();
+      this._initStyle();
+    }
+  }, {
+    key: '_initStyle',
+    value: function _initStyle() {
+      mapStyleToElement(this.el, this.options.style);
+    }
   }, {
     key: '_createElement',
     value: function _createElement() {
-      if (!isHTMLElement(this.el)) {
-        this.el = document.createElement('div');
-        document.getElementsByTagName('body')[0].appendChild(this.el);
-      }
+      this.el = document.createElement('div');
+      document.getElementsByTagName('body')[0].appendChild(this.el);
+      this.el.style.position = 'fixed';
+      this.el.style.top = 0;
+      this.el.style.left = 0;
+      this.el.style.right = 0;
+      mapStyleToElement(this.el, this.options);
     }
   }, {
     key: 'start',
--- loading-bar.js
+++ loading-bar.js
@@ -1,22 +1,45 @@
 const isHTMLElement = el => el instanceof HTMLElement;
 
+const mapStyleToElement = (el, style) => {
+  for (let prop in style) {
+    el.style[prop] = style[prop];
+  }
+};
+
 class LoadingBar {
   constructor(el, options) {
     this.el = typeof el === 'string' ? document.querySelector(el) : el;
-    this.options = options || {};
+
+    const defaultOptions = {
+      style: {
+        height: '4px',
+        backgroundColor: 'orange'
+      }
+    };
+
+    this.options = Object.assign({}, defaultOptions, options);
 
     this._init();
   }
 
   _init() {
+    !isHTMLElement(this.el) && this._createElement();
+    this._initStyle();
+  }
 
+  _initStyle() {
+    mapStyleToElement(this.el, this.options.style);
   }
 
   _createElement() {
-    if (!isHTMLElement(this.el)) {
-      this.el = document.createElement('div');
-      document.getElementsByTagName('body')[0].appendChild(this.el);
-    }
+    this.el = document.createElement('div');
+    document.getElementsByTagName('body')[0].appendChild(this.el);
+    this.el.style.position = 'fixed';
+    this.el.style.top = 0;
+    this.el.style.left = 0;
+    this.el.style.right = 0;
+    mapStyleToElement(this.el, this.options);
+
   }
 
   start() {
